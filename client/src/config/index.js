267275cb8a2a0d21ba4505652be5567f0ba0f77f
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2020-01-18 23:14:08 +0000

    minor mods

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://movie-mania-20.herokuapp.com/"
-// export const API_BASE_URL = "http://localhost:3100/"
+//export const API_BASE_URL = "https://movie-mania-20.herokuapp.com/"
+export const API_BASE_URL = "http://localhost:3100/"
