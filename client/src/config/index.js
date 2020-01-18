b716a7f33a96a2017a5dfcfddd53cf6d0b710b33
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2020-01-18 14:57:12 +0000

    minor mods

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = "https://turing-challenge-api.herokuapp.com/api/v1/"
-export const API_BASE_URL = "http://localhost:3100/"
+export const API_BASE_URL = "https://movie-mania-20.herokuapp.com/api/v1/"
+// export const API_BASE_URL = "http://localhost:3100/"
